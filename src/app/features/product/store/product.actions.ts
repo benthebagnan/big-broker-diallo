5ded570ab1d849e38069306fa52376c41aad319f
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-12-10 19:48:15 +0000

    Add action group

--- src/app/features/product/store/product.actions.ts
+++ src/app/features/product/store/product.actions.ts
@@ -1,3 +1,18 @@
-import { createAction, props } from '@ngrx/store';
+import {
+  createAction,
+  createActionGroup,
+  emptyProps,
+  props,
+} from '@ngrx/store';
+import { Product } from './product.reducer';
 
 export const getProductsRequest = createAction('[Product Compnent] Product');
+
+const productActions = createActionGroup({
+  source: 'Product',
+  events: {
+    'Get Products Request': emptyProps(),
+    'Get Products Success': props<{ products: Product[] }>(),
+    'Get Products Failure': props<{ error: string }>(),
+  },
+});
